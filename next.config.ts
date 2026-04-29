c625f06a68f1a647ac890996a2248b87b2966407
mianasimyaqoob <asimyaqoobmian@gmail.com>
2026-04-29 23:22:43 +0500

    Switch to Standalone mode for Hostinger Node.js deployment

--- next.config.ts
+++ next.config.ts
@@ -2,15 +2,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: 'export',
+  output: 'standalone',
   images: {
     unoptimized: true,
-  },
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  eslint: {
-    ignoreDuringBuilds: true,
   },
 };
 
